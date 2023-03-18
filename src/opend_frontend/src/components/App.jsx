ea94f02859977422fe9c31f74f1bec2d229a32b3
Raul Ceron <receronp@gmail.com>
2023-03-18 12:59:48 -0600

    Updates frontend to view minted NFTs.

--- src/opend_frontend/src/components/App.jsx
+++ src/opend_frontend/src/components/App.jsx
@@ -3,12 +3,15 @@ import Header from "./Header";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import homeImage from "../../assets/home-img.png";
+import Item from "./Item";
 
 function App() {
+  const NFT_ID = "rkp4c-7iaaa-aaaaa-aaaca-cai";
   return (
     <div className="App">
       <Header />
-      <img className="bottom-space" src={homeImage} />
+      <Item id={NFT_ID} />
+      {/* <img className="bottom-space" src={homeImage} /> */}
       <Footer />
     </div>
   );
